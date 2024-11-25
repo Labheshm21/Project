2175f98f3b257c93ca8fdedaad945a0d806486f2
Labhesh Mahajan <lmahajan@wpi.edu>
2024-11-25 17:57:07 -0500

    2nd commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,10 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
+    <Provider store={store}>
     <App/>
+    </Provider>
+    
     </BrowserRouter>
   
 )
